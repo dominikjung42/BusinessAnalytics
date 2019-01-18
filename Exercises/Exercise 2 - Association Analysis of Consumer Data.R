a2108ef94238e8ed2ee6e01ce464f91c62c7d488
Fiddleman <d.jung@kit.edu>
2019-01-18 10:25:14 +0100

    added code and slides for exercise 2

--- Exercises/Exercise 2 - Association Analysis of Consumer Data.R
+++ Exercises/Exercise 2 - Association Analysis of Consumer Data.R
@@ -2,33 +2,41 @@
 # Contact: Dominik Jung, d.jung@kit.edu
 
 library(arules)
-library(arulesViz)
-
 # Example of an association rule
-
-
-# Load the data
-shopping.data = read.transactions("scottish-supermarket.csv", format = "basket", sep=",", skip = 1)
-
-# Create an item frequency plot for the top 20 items
-itemFrequencyPlot(shopping.data,topN=20,type="absolute")
-summary(shopping.data)
-
-shopping.data = read.transactions("scottish-supermarket.csv", format = "basket", sep=",", rm.duplicates = FALSE, skip = 1)
-
-
-rules<-apriori(data=shopping.data, parameter=list(supp=0.001,conf = 0.001), 
-               appearance = list(default="lhs",rhs="whisky"),
-               control = list(verbose=F))
-rules<-sort(rules, decreasing=TRUE,by="confidence")
+data("Groceries")  # a popular dataset with shopping transactions
+rules = apriori(Groceries, parameter = list(supp=0.001))
+# itemFrequency(items(fit))  # use it to get percentage of items of the model
+summary(rules)
 inspect(rules)
 
-plot(rules[1:5])
+# Example visualization of assoication rules
+library(arulesViz)
+plot(rules)
+plot(rules[1:5], interactive=TRUE, shading=NA)
+plot(rules[1:5], method="graph", control=list(type="itemsets"))
+plot(rules[1:5], method="paracoord")
+ruleExplorer(rules[1:5], parameter = NULL)
+
+# Case
+# Load the data
+shoppingData = read.transactions("scottish-supermarket.csv", format = "basket", sep=",", skip = 1)
+
+library(arulesViz)
+# Create an item frequency plot for the top 20 items
+itemFrequencyPlot(shoppingData,topN=20,type="absolute")
+summary(shoppingData)
+
+rules = apriori(data=shoppingData, parameter=list(supp=0.001,conf = 0.001), 
+               appearance = list(rhs="whisky"),
+               control = list(verbose=F))
+
+rules = sort(rules, decreasing=TRUE,by="confidence")
+inspect(rules)
 
 plot(rules[1:5], interactive=TRUE, shading=NA)
 plot(rules[1:5], method="graph", control=list(type="itemsets"))
 
-
+ruleExplorer(rules[1:5], parameter = NULL)
 
 
 
